c05160cdfdc234164000e5515d051e85ac1d5b32
Anog R <38988046+ejnij@users.noreply.github.com>
2018-09-30 15:12:09 +0300

    Fixed Editor minimum size and made it so it only appears when a Manager node is selected.

--- addons/dialog_graph/DialogGraphPlugin.gd
+++ addons/dialog_graph/DialogGraphPlugin.gd
@@ -2,13 +2,30 @@ tool
 extends EditorPlugin
 
 var editor
+var manager = preload("DialogManager.gd")
+var button
+var selection
 
 func _enter_tree():
 	editor = preload("DialogGraphEditor.tscn").instance()
-	add_control_to_bottom_panel(editor, "Dialog Graph Editor")
-	add_custom_type("DialogManager", "Node", preload("DialogManager.gd"), preload("icon.png"))
+	button = add_control_to_bottom_panel(editor, "Dialog Graph Editor")
+	button.visible = false
+	add_custom_type("DialogManager", "Node", manager, preload("icon.png"))
+	selection = get_editor_interface().get_selection()
+	selection.connect("selection_changed", self, "_on_EditorInterface_selection_changed")
+	_on_EditorInterface_selection_changed()
+
+func _on_EditorInterface_selection_changed():
+	pass
+	button.visible = false
+	for node in selection.get_transformable_selected_nodes():
+		if node.get_script() and (node.get_script() == manager or node.get_script().get_base_script() == manager):
+			button.visible = true
+			break
 
 func _exit_tree():
+	selection.disconnect("selection_changed", self, "_on_EditorInterface_selection_changed")
+	button.visible = true
 	remove_control_from_bottom_panel(editor)
 	remove_custom_type("DialogManager")
 	editor.free()
